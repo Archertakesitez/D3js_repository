67b0a8665336747282a01c663d0da8a5e73bbbb5
Erchi Zhang <erchizhang@brandeis.edu>
2024-03-01 01:19:12 -0500

    add files

---
+++ visML_HW2.js
@@ -0,0 +1,34 @@
+//DO NOT CHANGE THIS FUNCTION
+function add_svg(wrapper) 
+{
+  var svg = d3.select(wrapper).select("svg");
+
+  if (svg.empty())
+    svg = d3.select(wrapper).append("svg");
+  else
+    svg.selectAll("*").remove();
+
+  return svg.attr("width", 300).attr("height", 300);
+}
+
+//DO NOT CHANGE THIS FUNCTION
+function set_update(div_id, _)
+{
+
+  comm.call({n: 5})
+  setInterval(function(){ comm.call({n: 5}) }, 2000);
+}
+
+function draw_circle(wrapper, data)
+{
+  let svg = add_svg(wrapper);
+
+  //TO DO 
+}
+
+function draw_regression(wrapper, data)
+{
+  let svg = add_svg(wrapper);
+
+  //TO DO 
+}
